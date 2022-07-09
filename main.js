a09c54363f3899dfbd35f72612c866c197692f5b
Nattaparin <nattaparin455@gmail.com>
2022-07-10 01:11:47 +0700

    3.5

--- main.js
+++ main.js
@@ -2,16 +2,17 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Shoes',
-            image: './assets/images/socks_green.jpg',
-            inStock: false,
-            inventory: 0,
+            brand: 'SE 331',
+
+            inventory: 100,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50},
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
             ],
-            cart: 0
-
+            cart: 0,
+            selectedVariant:0,
+            onSale: true
         }
     },
     methods: {
@@ -20,8 +21,26 @@ const app = Vue.createApp({
         },
         updateImage(variantImage) {
             this.image = variantImage
+        },
+        updateVariant(index) {
+            this.SelectedVariant = index;
         }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
+        onSaleja() {
+            return this.brand + ' is on sale ' + this.product
+        }
+
     }
 
-    
 })
